d13da80b96b19444ba263dc9d546dc551c04f510
Gnjurats <brandstattern@gmail.com>
2024-12-03 15:46:09 +0100

    Commit changes

--- src/components/Public.js
+++ src/components/Public.js
@@ -9,10 +9,11 @@ const Public = () => {
             <main className="public__main">
                 <p>Créée en 1926 sous l’appellation Association romande des arbitres de Football, l’association faitière des hommes en noir n’a cessé depuis cette date, de soutenir et d’appuyer les arbitres dans la pratique de leur passion.</p>
                 <address className="public__addr">
-                    Dan D. Repairs<br />
-                    555 Foo Drive<br />
-                    Foo City, CA 12345<br />
-                    <a href="tel:+15555555555">(555) 555-5555</a>
+                64, route du Grand-Lancy<br />
+                1212 Lancy<br />
+                (Tram 15, arrêt Lancy-Mairie)
+                Derrière la salle communale<br />
+                <a href="mailto:info@gag-ge.ch">info@gag-ge.ch</a>
                 </address>
                 <br />
                 <p>Owner: Marco Oliveira</p>
